2e8f9d9f8d848e6bb260a580a1bd3263253a2bd2
danihra-codurance <daniel.rodriguez@codurance.com>
2022-01-25 11:53:24 +0100

    red

--- src/test/java/romannumerals/RomanNumeralsShould.java
+++ src/test/java/romannumerals/RomanNumeralsShould.java
@@ -15,7 +15,7 @@ class RomanNumeralsShould {
         "4, IV",
         "5, V",
         "6, VI"
-K   })
+   })
     void convert_number_to_roman_numeral(int number, String romanNumeral) {
         RomanNumerals romanNumerals = new RomanNumerals();
 
