38b52d01543985d13529c43bad65c77e4dd49695
iagardi <garbigz@gmail.com>
2022-01-25 09:45:37 +0000

    green

--- src/main/java/romannumerals/RomanNumerals.java
+++ src/main/java/romannumerals/RomanNumerals.java
@@ -2,16 +2,7 @@ package romannumerals;
 
 public class RomanNumerals {
     public String convert(int input) {
-        StringBuilder result = new StringBuilder();
 
-        for (int i = 1; i<=input; i++) {
-            result.append("I");
-        }
-//
-//        if(input == 2){
-//            return "II";
-//        }
-
-        return result.toString();
+        return "I".repeat(input);
     }
 }
