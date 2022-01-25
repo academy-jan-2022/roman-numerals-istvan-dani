c55f8e2e6c0d6776fb49b0ea135d6c83d787e12e
danihra-codurance <daniel.rodriguez@codurance.com>
2022-01-25 10:39:49 +0100

    red

--- src/main/java/romannumerals/RomanNumerals.java
+++ src/main/java/romannumerals/RomanNumerals.java
@@ -2,6 +2,10 @@ package romannumerals;
 
 public class RomanNumerals {
     public String convert(int input) {
+        if(input == 2){
+            return "II";
+        }
+
         return "I";
     }
 }
--- src/test/java/romannumerals/RomanNumeralsShould.java
+++ src/test/java/romannumerals/RomanNumeralsShould.java
@@ -9,14 +9,28 @@ class RomanNumeralsShould {
     @Test
     void convert_1_to_I() {
         RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("I",romanNumerals.convert(1));
+
+        String result = romanNumerals.convert(1);
+
+        assertEquals("I", result);
     }
 
     @Test
     void convert_2_to_II() {
         RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("II", romanNumerals.convert(2));
+        String result = romanNumerals.convert(2);
+
+        assertEquals("II", result);
+    }
+
+    @Test
+    void convert_3_to_III() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+
+        String result = romanNumerals.convert(3);
+
+        assertEquals("III", result);
     }
 
 }
