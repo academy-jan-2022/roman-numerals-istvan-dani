c864ea16b41e53f3b7267d47da0e5c256cfbd383
danihra-codurance <daniel.rodriguez@codurance.com>
2022-01-25 11:53:02 +0100

    red

--- src/main/java/romannumerals/RomanNumerals.java
+++ src/main/java/romannumerals/RomanNumerals.java
@@ -2,8 +2,22 @@ package romannumerals;
 
 public class RomanNumerals {
     public String convert(int number) {
-        if (number == 4) return "IV";
-        return "I".repeat(number);
+        StringBuilder result = new StringBuilder();
 
+        while(number > 0){
+            if(number == 5){
+                result.append("V");
+                number -= 5;
+            }
+            if (number == 4){
+                result.append("IV");
+                number -= 4;
+            }
+            if(number >= 1){
+                result.append("I");
+                number-= 1;
+            }
+        }
+        return result.toString();
     }
 }
--- src/test/java/romannumerals/RomanNumeralsShould.java
+++ src/test/java/romannumerals/RomanNumeralsShould.java
@@ -13,8 +13,9 @@ class RomanNumeralsShould {
         "2, II",
         "3, III",
         "4, IV",
-        "5, V"
-    })
+        "5, V",
+        "6, VI"
+K   })
     void convert_number_to_roman_numeral(int number, String romanNumeral) {
         RomanNumerals romanNumerals = new RomanNumerals();
 
